55d5b75b123d785387da395d4608d66534c7b43b
Mayowa 'Mylo' Akinrodoye <makin24@terpmail.umd.edu>
2025-11-18 19:04:05 -0500

    Add method signatures for library functions

--- src/method_signatures.py
+++ src/method_signatures.py
@@ -1,1 +1,10 @@
 
+
+def validate_member_id(member_id)
+def calculate_due_date(checkout_date, loan_period)
+def compute_overdue_fine(due_date, return_date, daily_rate)
+def check_item_availability(item_id, catalog)
+def format_item_display(item_data)
+def generate_checkout_record(member_id, item_id, checkout_date)
+def search_catalog(query, catalog_data)
+def update_item_status(item_id, new_status)
