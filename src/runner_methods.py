29c714429c54e6c240a81daf20b5d1542e0bcaf6
Mayowa 'Mylo' Akinrodoye <makin24@terpmail.umd.edu>
2025-12-13 17:13:01 -0500

    Initialize catalog and load sample data
    Initialize catalog and load sample data into it.

--- src/runner_methods.py
+++ src/runner_methods.py
@@ -1,14 +1,23 @@
-
 # runner_file.py
 
 from data.sample_data import SAMPLE_BOOKS
+from runner_methods import load_sample_data_into_catalog
+from models.catalog import Catalog
+
 
 def run():
     """
     Entry point:
-    - Load installed data
-    - Initialize domain objects
+    - Initialize catalog
+    - Load installed sample data
     - Start dashboard workflow
     """
-    pass
+    catalog = Catalog("Main Library Catalog")
 
+    load_sample_data_into_catalog(
+        catalog=catalog,
+        sample_records=SAMPLE_BOOKS
+    )
+
+    # dashboard / CLI would start here
+    pass
